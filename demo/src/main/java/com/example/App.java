8aa4669320334cda2677309cda6aced7e2adbf2f
Leonardo Ferreira <leoventura245@gmail.com>
2025-01-20 00:11:38 -0300

    projeto finalizado e testado

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,9 +1,19 @@
 package com.example;
 
+import com.example.services.CurrencyConverter;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        String fromCurrency = "USD";
+        String toCurrency = "BRL";
+        double amount = 100.0;
+
+        try {
+            double convertedAmount = CurrencyConverter.converter(fromCurrency, toCurrency, amount);
+            System.out.printf("%.2f %s é equivalente a %.2f %s%n", amount, fromCurrency, convertedAmount, toCurrency);
+        } catch (Exception e) {
+            System.err.println("Erro ao converter moedas: " + e.getMessage());
+        }
     }
 }
